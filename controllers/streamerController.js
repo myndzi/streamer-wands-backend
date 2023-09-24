385b5e24e796a468c75be072ad60f5da08851916
Kris Reeves <github.com@masquerader.com>
2023-09-24 18:03:44 +0000

    Remove no-longer-used files

--- controllers/streamerController.js
+++ controllers/streamerController.js
@@ -1,33 +1,6 @@
 const mongoose = require('mongoose')
 const Streamers = mongoose.model('Streamers')
-/* obsolete
-const spellData = require("../utils/spellData")
-const wandSprites = require("../utils/wandSprites")
 
-function usedWands(wands) {
-    const sprites = {}
-    wands.forEach(wand => {
-        const sprite = wand.stats.sprite
-        sprites[sprite] = wandSprites[sprite]
-    })
-    return sprites
-}
-
-function usedSpells(wands) {
-    const spells = {}
-    wands.forEach(wand => {
-        for (const spell of wand.always_cast) {
-            if (!spells[spell])
-                spells[spell] = spellData[spell]
-        }
-        for (const spell of wand.deck) {
-            if (!spells[spell])
-                spells[spell] = spellData[spell]
-        }
-    })
-    return spells
-}
-*/
 exports.getWands = async (req, res, next) => {
     const { name } = req.params
     const streamer = await Streamers.findOne({ name }).collation({
