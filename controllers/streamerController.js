4f39b9f8898e0d4eb3e39d49b774d0eb7036949b
Kris Reeves <github.com@masquerader.com>
2023-09-24 18:55:00 +0000

    Add cache busting to wand/spell data

--- controllers/streamerController.js
+++ controllers/streamerController.js
@@ -1,6 +1,22 @@
 const mongoose = require('mongoose')
 const Streamers = mongoose.model('Streamers')
 
+const { statSync } = require('node:fs')
+const mtime = {
+    spellData: statSync(
+        __dirname,
+        '..',
+        'public',
+        'spellData.js',
+    ).mtime.getTime(),
+    wandSprites: statSync(
+        __dirname,
+        '..',
+        'public',
+        'wandSprites.js',
+    ).mtime.getTime(),
+}
+
 exports.getWands = async (req, res, next) => {
     const { name } = req.params
     const streamer = await Streamers.findOne({ name }).collation({
@@ -8,14 +24,14 @@ exports.getWands = async (req, res, next) => {
         strength: 1,
     })
     if (streamer) {
-        const stuff = {
+        res.render('wands', {
+            mtime,
             menu: false,
             title: `${streamer.name} wands`,
             streamer: streamer.name,
             wands: streamer.wands,
             inventory: streamer.inventory || [],
-        }
-        res.render('wands', stuff)
+        })
     } else {
         next()
     }
