49195670ffcb8951f2051564bcab6a1de3067a60
teh60 <unconfigured@null.spigotmc.org>
2025-03-16 23:43:10 -0700

    fixed mod settings not updating

--- mod/settings.lua
+++ mod/settings.lua
@@ -2,33 +2,38 @@ dofile("data/scripts/lib/mod_settings.lua")
 
 local mod_id = "streamer_wands"
 
-mod_settings_version = 1
+mod_settings_version = 2
 mod_settings =
 {
     {
         id = "seed",
         ui_name = "Show Seed",
         value_default = true,
+        scope = MOD_SETTING_SCOPE_RUNTIME
     },
     {
         id = "position",
         ui_name = "Show Position/Coordinates",
         value_default = true,
+        scope = MOD_SETTING_SCOPE_RUNTIME
     },
     {
         id = "ngp",
         ui_name = "Show New Game Plus",
         value_default = true,
+        scope = MOD_SETTING_SCOPE_RUNTIME
     },
     {
         id = "shifts",
         ui_name = "Show Shifts",
         value_default = true,
+        scope = MOD_SETTING_SCOPE_RUNTIME
     },
     {
         id = "shiftsTimer",
         ui_name = "Show Shift Timer",
         value_default = true,
+        scope = MOD_SETTING_SCOPE_RUNTIME
     },
 }
 
