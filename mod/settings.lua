83861c5ed0b8a4c0175458e57d7ac0f782e5ca13
teh60 <unconfigured@null.spigotmc.org>
2025-03-19 16:10:55 -0700

    fixed wrong mod settings categories

--- mod/settings.lua
+++ mod/settings.lua
@@ -8,53 +8,55 @@ mod_settings =
     {
         category_id = "notice",
         ui_name = [[
-            Show Run Seed requires a mod restart/new game with mods enabled
-            in order to hide or unhide the seed from pause menu and loading text
+    Show Run Seed requires a mod restart/new game with mods enabled
+    in order to hide or unhide the seed from pause menu and loading text
 
-            All mod settings will hide/show on onlywands.com at the same frequency
-            as any other onlywands data.
-            ]],
+    All mod settings will hide/show on onlywands.com at the same frequency
+    as any other onlywands data.
+    ]],
         settings = {},
     },
     {
-        id = "seed",
-        ui_name = "Show Run Seed",
-        ui_description =
-        "Show Run Seed on onlywands.com, on pause menu, and on loading text",
-        value_default = true,
-        scope = MOD_SETTING_SCOPE_RUNTIME
-    },
-    {
-        id = "position",
-        ui_name = "Show Current Position",
-        ui_description = "All of these toggles will update like any other onlywands data",
-        value_default = true,
-        scope = MOD_SETTING_SCOPE_RUNTIME
-    },
-    {
-        id = "ngp",
-        ui_name = "Show Current New Game Plus Number",
-        value_default = true,
-        scope = MOD_SETTING_SCOPE_RUNTIME
-    },
-    {
-        id = "shifts",
-        ui_name = "Show Shifts",
-        ui_description =
-        "List current run's fungal shifts, cheatgui/other modded fungal shifts don't play nice with grouping shifts",
-        value_default = true,
-        scope = MOD_SETTING_SCOPE_RUNTIME
-    },
-    {
-        id = "shiftsTimer",
-        ui_name = "Show Shift Timer",
-        ui_description = "Show how long until next fungal shift",
-        value_default = true,
-        scope = MOD_SETTING_SCOPE_RUNTIME
-    },
-    {
-        category_id = "disclaimer"
-
+        category_id = "feature_toggles",
+        ui_name = "Feature Toggles",
+        settings = {
+            {
+                id = "seed",
+                ui_name = "Show Run Seed",
+                ui_description =
+                "Show Run Seed on onlywands.com, on pause menu, and on loading text",
+                value_default = true,
+                scope = MOD_SETTING_SCOPE_RUNTIME
+            },
+            {
+                id = "position",
+                ui_name = "Show Current Position",
+                ui_description = "Also pauses map + parallel world tracking",
+                value_default = true,
+                scope = MOD_SETTING_SCOPE_RUNTIME
+            },
+            {
+                id = "ngp",
+                ui_name = "Show Current New Game Plus Number",
+                value_default = true,
+                scope = MOD_SETTING_SCOPE_RUNTIME
+            },
+            {
+                id = "shifts",
+                ui_name = "Show Shifts",
+                ui_description =
+                "List current run's fungal shifts, cheatgui/other modded fungal shifts don't play nice with grouping shifts",
+                value_default = true,
+                scope = MOD_SETTING_SCOPE_RUNTIME
+            },
+            {
+                id = "shiftsTimer",
+                ui_name = "Show Shift Timer",
+                ui_description = "Show how long until next fungal shift",
+                value_default = true,
+                scope = MOD_SETTING_SCOPE_RUNTIME
+            },
+        },
     },
 }
 
